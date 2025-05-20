34e6a30ce5d97478492a4a34d2e82aef458991b5
StefoniMircea <mircea.stefoni82@yahoo.ro>
2025-05-20 20:05:33 +0300

    Fixed layout import x2

--- vibecheck-frontend/src/components/layout/Layout.tsx
+++ vibecheck-frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
-import Header from '../Layout/Header/Header';
-import Footer from '../Layout/Footer/Footer';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 import './Layout.css';
 
 interface LayoutProps {
